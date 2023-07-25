244e156f3bbfd38a6373880ee7e1c565a50981b3
steevecode <stevecode@yahoo.com>
2023-07-25 19:39:14 +0100

    Step 08 - Exploring Spring Security - Cross Site Request Forgery - CSRF

--- src/main/java/com/brexson/learnspringsecurity/resources/HelloWorldResource.java
+++ src/main/java/com/brexson/learnspringsecurity/resources/HelloWorldResource.java
@@ -1,4 +1,4 @@
-package com.brexson.learnspringsecurity;
+package com.brexson.learnspringsecurity.resources;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
